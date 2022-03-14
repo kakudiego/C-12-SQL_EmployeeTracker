aedbe0f4ccd782db67f6c41329cff7506c57949c
Diego Kaku <diegokaku@gmail.com>
2022-03-13 21:14:10 -0500

    schema and seed done

---
+++ index.js
@@ -0,0 +1,215 @@
+// Function for inquirer to prompt data
+function runSearch() {
+  inquirer
+    .prompt({
+      name: 'questions',
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View all employees',
+        'View all employees by department',
+        'View all employees by manager',
+        'Add employee',
+        'Remove employee',
+        'Update employee role',
+        'Update employee manager',
+        // 'View all Roles',
+        'Add Role',
+        // 'Remove role',
+        // 'View all departments',
+        'Add Department',
+        // 'Remove department'
+        // 'View total utilized budget by department',
+        // 'Quit'
+      ],
+      // Promise
+    })
+    .then((answers) => {
+      // Start switch statement
+      switch (answers.questions) {
+        // Start new case
+        case 'View all employees':
+          byEmployees();
+          runSearch();
+
+          break;
+        // Start new case
+        case 'View all employees by department':
+          byDepartment();
+          runSearch();
+
+          break;
+
+        case 'View all employees by manager':
+          byManager();
+          runSearch();
+
+          break;
+
+        // Start new case
+        // Takes further input
+        case 'Add employee':
+          inquirer
+            .prompt([
+              {
+                name: 'employeeFirst',
+                type: 'input',
+                message: "What is the employee's first name?",
+                validate: (answer) => {
+                  if (answer !== '') {
+                    return true;
+                  }
+                  return 'Please enter at least one character.';
+                },
+              },
+              {
+                name: 'employeeLast',
+                type: 'input',
+                message: "What is the employee's last name?",
+                validate: (answer) => {
+                  if (answer !== '') {
+                    return true;
+                  }
+                  return 'Please enter at least one character.';
+                },
+              },
+              {
+                name: 'department',
+                type: 'input',
+                message: 'Please enter the role id',
+              },
+              {
+                name: 'manager',
+                type: 'input',
+                message: 'Please enter manager id',
+              },
+            ])
+            .then((answers) => {
+              addEmployee(answers.employeeFirst, answers.employeeLast, answers.department, answers.manager);
+              runSearch();
+            });
+
+          break;
+        // Start new case
+        // Takes further input
+        case 'Add Department':
+          inquirer
+            .prompt([
+              {
+                name: 'Department',
+                type: 'input',
+                message: 'Please enter the department you would like to add?',
+                validate: (answer) => {
+                  if (answer !== '') {
+                    return true;
+                  }
+                  return 'Please enter at least one character.';
+                },
+              },
+            ])
+            .then((answers) => {
+              // Adds department to database
+              addDepartment(answers.Department);
+              runSearch();
+            });
+          break;
+        // Start new case
+        // Takes further input
+        case 'Add Role':
+          inquirer
+            .prompt([
+              {
+                name: 'title',
+                type: 'input',
+                message: "Please enter the role's title.",
+                validate: (answer) => {
+                  if (answer !== '') {
+                    return true;
+                  }
+                  return 'Please enter at least one character.';
+                },
+              },
+              {
+                name: 'salary',
+                type: 'input',
+                message: "Please enter the role's salary.",
+              },
+              {
+                name: 'department_id',
+                type: 'input',
+                message: 'Please enter the department id.',
+              },
+            ])
+            .then((answers) => {
+              // Adds role to database
+              addRole(answers.title, answers.salary, answers.department_id);
+              runSearch();
+            });
+          break;
+
+        // Start new case
+        // Takes further input
+        case 'Remove employee':
+          inquirer
+            .prompt([
+              {
+                name: 'id',
+                type: 'input',
+                message: 'Please enter the Employee id',
+              },
+            ])
+            .then((answers) => {
+              // Removes employee to database
+              removeEmployee(answers.id);
+              runSearch();
+            });
+          break;
+
+        // Start new case
+        case 'Update employee role':
+          inquirer
+            .prompt([
+              {
+                name: 'employeeId',
+                type: 'input',
+                message: "Please enter employee's id",
+              },
+              {
+                name: 'roleId',
+                type: 'input',
+                message: "Please enter role's id",
+              },
+            ])
+            .then((answers) => {
+              // Updates employee's role
+              updateByRole(answers.employeeId, answers.roleId);
+              runSearch();
+            });
+
+          break;
+        // Start new case
+        // Takes further input
+        case 'Update employee manager':
+          inquirer
+            .prompt([
+              {
+                name: 'manager',
+                type: 'input',
+                message: 'Please enter manager id',
+              },
+              {
+                name: 'Employee',
+                type: 'input',
+                message: 'Please enter employee id',
+              },
+            ])
+            .then((answers) => {
+              // Updates employee's manager
+              updateByManager(answers.manager, answers.Employee);
+              runSearch();
+            });
+
+          break;
+      }
+    });
+}
