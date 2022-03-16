e5f59fe3f9c05efe25a0c3020c664d1754214ef5
Diego Kaku <diegokaku@gmail.com>
2022-03-15 21:32:14 -0500

    fix table display

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const db = require('./db/connection');
 // const PORT = process.env.PORT || 3002;
 // const app = express();
 
-function init() {
-  // console.clear();
-  console.log(`
-  =======================================
-  =                                     =
-  =           Employee Manager          =
-  =                                     =
-  =======================================
-  `);
-
-  firstPrompt();
-}
-
-// Function for inquirer to prompt data
+// function inquirer/prompt question
 function firstPrompt() {
+  db.connect((err) => {
+    if (err) throw err;
+  });
+
+  console.log(`
+    ==============================
+    =                            =
+    =      Employee Manager      =
+    =                            =
+    ==============================
+    `);
+
   inquirer
     .prompt({
       name: 'questions',
@@ -53,10 +52,14 @@ function firstPrompt() {
           allDepartments();
           firstPrompt();
 
+          break;
+
         // MVP
         case 'View all Roles':
           allRoles();
           firstPrompt();
+
+          break;
 
         // MVP
         case 'View all employees':
@@ -65,128 +68,128 @@ function firstPrompt() {
 
           break;
 
-        // MVP
-        case 'Add Department':
-          inquirer
-            .prompt([
-              {
-                name: 'Department',
-                type: 'input',
-                message: 'Please enter the department you would like to add?',
-                validate: (answer) => {
-                  if (answer !== '') {
-                    return true;
-                  }
-                  return 'Please enter at least one character.';
-                },
-              },
-            ])
-            .then((answers) => {
-              // Adds department to database
-              addDepartment(answers.Department);
-              firstPrompt();
-            });
-          break;
+        // // MVP
+        // case 'Add Department':
+        //   inquirer
+        //     .prompt([
+        //       {
+        //         name: 'Department',
+        //         type: 'input',
+        //         message: 'Please enter the department you would like to add?',
+        //         validate: (answer) => {
+        //           if (answer !== '') {
+        //             return true;
+        //           }
+        //           return 'Please enter at least one character.';
+        //         },
+        //       },
+        //     ])
+        //     .then((answers) => {
+        //       // Adds department to database
+        //       addDepartment(answers.Department);
+        //       firstPrompt();
+        //     });
+        //   break;
 
-        // MVP
-        case 'Add Role':
-          inquirer
-            .prompt([
-              {
-                name: 'title',
-                type: 'input',
-                message: "Please enter the role's title.",
-                validate: (answer) => {
-                  if (answer !== '') {
-                    return true;
-                  }
-                  return 'Please enter at least one character.';
-                },
-              },
-              {
-                name: 'salary',
-                type: 'input',
-                message: "Please enter the role's salary.",
-              },
-              {
-                name: 'department_id',
-                type: 'input',
-                message: 'Please enter the department id.',
-              },
-            ])
-            .then((answers) => {
-              // Adds role to database
-              addRole(answers.title, answers.salary, answers.department_id);
-              firstPrompt();
-            });
-          break;
+        // // MVP
+        // case 'Add Role':
+        //   inquirer
+        //     .prompt([
+        //       {
+        //         name: 'title',
+        //         type: 'input',
+        //         message: "Please enter the role's title.",
+        //         validate: (answer) => {
+        //           if (answer !== '') {
+        //             return true;
+        //           }
+        //           return 'Please enter at least one character.';
+        //         },
+        //       },
+        //       {
+        //         name: 'salary',
+        //         type: 'input',
+        //         message: "Please enter the role's salary.",
+        //       },
+        //       {
+        //         name: 'department_id',
+        //         type: 'input',
+        //         message: 'Please enter the department id.',
+        //       },
+        //     ])
+        //     .then((answers) => {
+        //       // Adds role to database
+        //       addRole(answers.title, answers.salary, answers.department_id);
+        //       firstPrompt();
+        //     });
+        //   break;
 
-        // MVP
-        case 'Add employee':
-          inquirer
-            .prompt([
-              {
-                name: 'employeeFirst',
-                type: 'input',
-                message: "What is the employee's first name?",
-                validate: (answer) => {
-                  if (answer !== '') {
-                    return true;
-                  }
-                  return 'Please enter at least one character.';
-                },
-              },
-              {
-                name: 'employeeLast',
-                type: 'input',
-                message: "What is the employee's last name?",
-                validate: (answer) => {
-                  if (answer !== '') {
-                    return true;
-                  }
-                  return 'Please enter at least one character.';
-                },
-              },
-              {
-                name: 'department',
-                type: 'input',
-                message: 'Please enter the role id',
-              },
-              {
-                name: 'manager',
-                type: 'input',
-                message: 'Please enter manager id',
-              },
-            ])
-            .then((answers) => {
-              addEmployee(answers.employeeFirst, answers.employeeLast, answers.department, answers.manager);
-              firstPrompt();
-            });
+        // // MVP
+        // case 'Add employee':
+        //   inquirer
+        //     .prompt([
+        //       {
+        //         name: 'employeeFirst',
+        //         type: 'input',
+        //         message: "What is the employee's first name?",
+        //         validate: (answer) => {
+        //           if (answer !== '') {
+        //             return true;
+        //           }
+        //           return 'Please enter at least one character.';
+        //         },
+        //       },
+        //       {
+        //         name: 'employeeLast',
+        //         type: 'input',
+        //         message: "What is the employee's last name?",
+        //         validate: (answer) => {
+        //           if (answer !== '') {
+        //             return true;
+        //           }
+        //           return 'Please enter at least one character.';
+        //         },
+        //       },
+        //       {
+        //         name: 'department',
+        //         type: 'input',
+        //         message: 'Please enter the role id',
+        //       },
+        //       {
+        //         name: 'manager',
+        //         type: 'input',
+        //         message: 'Please enter manager id',
+        //       },
+        //     ])
+        //     .then((answers) => {
+        //       addEmployee(answers.employeeFirst, answers.employeeLast, answers.department, answers.manager);
+        //       firstPrompt();
+        //     });
 
-          break;
+        //   break;
 
-        // MVP
-        case 'Update employee role':
-          inquirer
-            .prompt([
-              {
-                name: 'employeeId',
-                type: 'input',
-                message: "Please enter employee's id",
-              },
-              {
-                name: 'roleId',
-                type: 'input',
-                message: "Please enter role's id",
-              },
-            ])
-            .then((answers) => {
-              // Updates employee's role
-              updateByRole(answers.employeeId, answers.roleId);
-              firstPrompt();
-            });
+        // // MVP
+        // case 'Update employee role':
+        //   inquirer
+        //     .prompt([
+        //       {
+        //         name: 'employeeId',
+        //         type: 'input',
+        //         message: "Please enter employee's id",
+        //       },
+        //       {
+        //         name: 'roleId',
+        //         type: 'input',
+        //         message: "Please enter role's id",
+        //       },
+        //     ])
+        //     .then((answers) => {
+        //       // Updates employee's role
+        //       updateByRole(answers.employeeId, answers.roleId);
+        //       firstPrompt();
+        //     });
 
-          break;
+        //   break;
 
         // // NOT MVP
         // case 'View all employees by department':
@@ -245,14 +248,32 @@ function firstPrompt() {
     });
 }
 
+// 'View all departments'
+function allDepartments() {
+  let departmentAll = db.query(
+    'SELECT id, department FROM department;',
+
+    function (error, departmentAll) {
+      if (error) throw error;
+
+      console.log('\n');
+
+      console.table(departmentAll);
+    }
+  );
+}
+
 // "View all employees",
 function allEmployees() {
-  let results = db.query(
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.d_name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
+  let employeesAll = db.query(
+    "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.department AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;",
 
-    function (error, results) {
+    function (error, employeesAll) {
       if (error) throw error;
-      console.table(results);
+
+      console.log('\n');
+
+      console.table(employeesAll);
     }
   );
 }
@@ -263,11 +284,34 @@ function allRoles() {
     'SELECT title, salary, department_id FROM role;',
 
     function (error, rolesAll) {
-      console.table(rolesAll);
       if (error) throw error;
+
+      console.log('\n');
+
+      console.table(rolesAll);
     }
   );
 }
+
+// // "Add Department"
+// function addDepartment(department) {
+//   let department = db.query('INSERT INTO department SET d_name = ?', [department], function (error, department) {
+//     if (error) throw error;
+//     // console.table(manager)
+//   });
+
+//   departmentTable();
+// }
+
+// // "Add role"
+// function addRole(title, salary, department_id) {
+//   let newRole = db.query('INSERT INTO role SET title = ?, salary = ?, department_id = ?', [title, salary, department_id], function (error, newRole) {
+//     if (error) throw error;
+//     // console.table(manager)
+//   });
+
+//   allRoles();
+// }
 
 // // "View all employees by department",
 // function allByDepartment() {
@@ -324,26 +368,6 @@ function allRoles() {
 //   );
 // }
 
-// // "Add Department"
-// function addDepartment(department) {
-//   let department = db.query('INSERT INTO department SET d_name = ?', [department], function (error, department) {
-//     if (error) throw error;
-//     // console.table(manager)
-//   });
-
-//   departmentTable();
-// }
-
-// // "Add role"
-// function addRole(title, salary, department_id) {
-//   let newRole = db.query('INSERT INTO role SET title = ?, salary = ?, department_id = ?', [title, salary, department_id], function (error, newRole) {
-//     if (error) throw error;
-//     // console.table(manager)
-//   });
-
-//   allRoles();
-// }
-
 // // "Remove employee"
 // function removeEmployee(id) {
 //   let add = db.query('DELETE FROM employee WHERE id = ?', [id], function (error, id) {
@@ -370,15 +394,4 @@ function allRoles() {
 
 // Connects mysql2 server and runs init on startup
 
-db.connect((err) => {
-  if (err) throw err;
-  console.log('Database connected.');
-  // app.listen(PORT, () => {
-  //   console.log(`
-  //   -
-  //   Server running on port ${PORT}
-  //   -
-  //   `);
-  // });
-  init();
-});
+firstPrompt();
